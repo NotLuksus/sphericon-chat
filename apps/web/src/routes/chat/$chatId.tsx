bfa2798edb9c7fb759eb87c84efa4e4b3dd5170f
Lukas Marienfeld <lukasmarienfeldlm@gmail.com>
2025-07-12 12:46:05 +0200

    wip

--- apps/web/src/routes/chat/$chatId.tsx
+++ apps/web/src/routes/chat/$chatId.tsx
@@ -7,52 +7,53 @@ import { last } from "effect/Array";
 import { useEffect } from "react";
 
 export const Route = createFileRoute("/chat/$chatId")({
-	component: RouteComponent,
+  component: RouteComponent,
 });
 
 function RouteComponent() {
-	const { chatId } = Route.useParams();
-	const z = useZ();
-	const [messages] = useQuery(
-		z.query.messagesTable.where("chatId", chatId).orderBy("createdAt", "asc"),
-	);
+  const { chatId } = Route.useParams();
+  const z = useZ();
+  const [messages] = useQuery(
+    z.query.messagesTable.where("chatId", chatId).orderBy("createdAt", "asc"),
+  );
 
-	const { message } = useStreamingMessage({ chatId });
+  const { message } = useStreamingMessage({ chatId });
 
-	useEffect(() => {
-		const lastMessage = last(messages);
-		Option.match(lastMessage, {
-			onNone: () => console.log("No message"),
-			onSome: (message) => {
-				if (message.status === "streaming") {
-					console.log("Streaming message");
-				}
-			},
-		});
-	}, [messages]);
-	return (
-		<div className="flex flex-col items-center">
-			<div className="flex flex-col items-center gap-4">
-				{messages.map((message) => (
-					<div className="bg-blue-600 px-8 py-2 flex-col" key={message.id}>
-						<p>{message.parentMessageId}</p>
-						<p>{message.content}</p>
-						<p>{message.id}</p>
-					</div>
-				))}
-			</div>
-			<div className="flex flex-col items-center">
-				<button
-					onClick={() => {
-						z.mutate.message.send({
-							chatId,
-							content: "Hey, how are you?",
-						});
-					}}
-				>
-					Send Message
-				</button>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    const lastMessage = last(messages);
+    Option.match(lastMessage, {
+      onNone: () => console.log("No message"),
+      onSome: (message) => {
+        if (message.status === "streaming") {
+          console.log("Streaming message");
+        }
+      },
+    });
+  }, [messages]);
+  return (
+    <div className="flex flex-col items-center">
+      <p>{message}</p>
+      <div className="flex flex-col items-center gap-4">
+        {messages.map((message) => (
+          <div className="bg-blue-600 px-8 py-2 flex-col" key={message.id}>
+            <p>{message.parentMessageId}</p>
+            <p>{message.content}</p>
+            <p>{message.id}</p>
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-col items-center">
+        <button
+          onClick={() => {
+            z.mutate.message.send({
+              chatId,
+              content: "Hey, how are you?",
+            });
+          }}
+        >
+          Send Message
+        </button>
+      </div>
+    </div>
+  );
 }
--- apps/web/src/services/common/apiClient.tsx
+++ apps/web/src/services/common/apiClient.tsx
@@ -1,26 +1,35 @@
 import * as FetchHttpClient from "@effect/platform/FetchHttpClient";
 import * as HttpApiClient from "@effect/platform/HttpApiClient";
 import * as HttpClient from "@effect/platform/HttpClient";
-import { Api } from "@sphericon/api";
+import { ClientApi } from "@sphericon/api/client";
 import * as Effect from "effect/Effect";
 import * as UnsafeHttpApiClient from "./unsafeHttpClient";
+import { Layer } from "effect";
+
+const CustomFetchLive = FetchHttpClient.layer.pipe(
+  Layer.provide(
+    Layer.succeed(FetchHttpClient.RequestInit, {
+      credentials: "include",
+    }),
+  ),
+);
 
 export class ApiClient extends Effect.Service<ApiClient>()("ApiClient", {
-	accessors: true,
-	dependencies: [FetchHttpClient.layer],
-	effect: Effect.gen(function* () {
-		return {
-			client: yield* HttpApiClient.make(Api, {
-				baseUrl: import.meta.env.VITE_SERVER_URL,
-				transformClient: (client) =>
-					client.pipe(HttpClient.retryTransient({ times: 3 })),
-			}),
+  accessors: true,
+  dependencies: [CustomFetchLive],
+  effect: Effect.gen(function* () {
+    return {
+      client: yield* HttpApiClient.make(ClientApi, {
+        baseUrl: import.meta.env.VITE_SERVER_URL,
+        transformClient: (client) =>
+          client.pipe(HttpClient.retryTransient({ times: 3 })),
+      }),
 
-			unsafeClient: yield* UnsafeHttpApiClient.make(Api, {
-				baseUrl: import.meta.env.VITE_SERVER_URL,
-				transformClient: (client) =>
-					client.pipe(HttpClient.retryTransient({ times: 3 })),
-			}),
-		};
-	}),
+      unsafeClient: yield* UnsafeHttpApiClient.make(ClientApi, {
+        baseUrl: import.meta.env.VITE_SERVER_URL,
+        transformClient: (client) =>
+          client.pipe(HttpClient.retryTransient({ times: 3 })),
+      }),
+    };
+  }),
 }) {}
